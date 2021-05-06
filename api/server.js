54ec6f711b24e69c8c59a2b10b1fb7a7cdc26949
Tony So <60905365+TonySo88@users.noreply.github.com>
2021-05-05 19:32:15 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session')
+const KnexSessionStore = require('connect-session-knex')(session)
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-router');
+const Knex = require("knex");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -20,6 +25,19 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(session({
+  resave: false,
+  saveUninitilized: false,
+  secret: "keep it secret keep it save",
+  store: new KnexSessionStore({
+    knex: db,
+    createtable: true
+  })
+}))
+
+server.use("/api/auth", authRouter)
+server.use("/api/users", usersRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
